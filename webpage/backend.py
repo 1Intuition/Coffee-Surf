7e60b7f50e5aad6467be77ede817afc443fac7a7
raharison-toky <raharisonrtoky@gmail.com>
2022-11-20 01:28:01 -0500

    backend

--- webpage/backend.py
+++ webpage/backend.py
@@ -3,6 +3,7 @@ import pickle
 app = Flask(__name__)
 
 data =  pickle.load(open("data.p", 'rb'))
+keys = pickle.load(open("keys.p", 'rb'))
 
 posts = list(data.values())[0:5]
 
@@ -15,6 +16,14 @@ def home():
 def product_page():
     return render_template("product.html",posts=posts)
 
+@app.route("/product/<coffee_name>")
+def coffee_page(coffee_name):
+    index = keys[coffee_name]
+    print(index)
+    recommendations = [data[index]]
+    recommendations.extend([data[i] for i in data[index]["recommendations"]])
+    print(recommendations[0])
+    return render_template("product.html",coffees=recommendations)
 
 if __name__ == "main":
     app.run(debug=True)
